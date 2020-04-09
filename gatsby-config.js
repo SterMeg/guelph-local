df4b74c4aa9fc7d39e517c7ad5b2e2f20d3edfd7
Meghan Sterling <sterling.meghan@gmail.com>
2020-04-09 10:52:27 -0400

    Add Gatsby Netlify plugin to fix cache issue

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,7 +8,7 @@ module.exports = {
   },
   plugins: [
     "gatsby-plugin-eslint",
-    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-netlify`,`gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
